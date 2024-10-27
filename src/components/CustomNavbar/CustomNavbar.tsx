f9cda95bb8ff5ce8f86a52a686312fb7d5ce2ab4
Mubashir Ali <ali.mubashir40@gmail.com>
2024-10-27 17:07:46 +0530

    [FIX]Sidebar UI Code

--- src/components/CustomNavbar/CustomNavbar.tsx
+++ src/components/CustomNavbar/CustomNavbar.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styles from "./CustomNavbar.module.scss";
 import { Button, Col, Row } from "reactstrap";
+import classNames from "classnames";
 
 interface CustomNavbarProps {
   onSave: () => void;
@@ -21,7 +22,18 @@ const CustomNavbar: React.FC<CustomNavbarProps> = ({
   return (
     <Row className={styles.navbar}>
       <Col md={10} className={styles.navbar_message}>
-        {submitMessage != null && (submitMessage ? "Success" : "Error")}
+        {submitMessage != null && (
+          <div
+            className={classNames(
+              styles.navbar_badge,
+              submitMessage
+                ? styles.navbar_badge_success
+                : styles.navbar_badge_danger
+            )}
+          >
+            {submitMessage ? "Success" : "Error"}
+          </div>
+        )}
       </Col>
       <Col md={2} className={styles.navbar_button}>
         <Button onClick={onSave}>Save Changes</Button>
