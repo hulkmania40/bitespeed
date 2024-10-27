d89b55154ffcded5fca57792a6e10422176c678d
Mubashir Ali <ali.mubashir40@gmail.com>
2024-10-27 17:01:35 +0530

    [FIX] Sidebar UI code

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import styles from "./Sidebar.module.scss";
-import { Input } from "reactstrap";
+import { Input, Label } from "reactstrap";
 import NodesPanel from "../NodesPanel/NodesPanel";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import classNames from "classnames";
 
 interface SidebarProps {
   updatedInputValue: (updatedVal: string) => void;
@@ -17,25 +18,28 @@ interface SidebarProps {
 const Sidebar = (props: SidebarProps) => {
   return (
     <div className={styles.sidebar}>
-      <h2 className={styles.title}>
+      <h2 className={styles.sidebar_title}>
         {props.nodesPanel ? (
           ""
         ) : (
           <FontAwesomeIcon
+            className={classNames("mr-3",styles.sidebar_icon)}
             icon={faCircleLeft}
             onClick={props.toggleNodesPanel} // Toggle nodes panel on click
           />
         )}
         {props.nodesPanel ? "Nodes Panel" : "Settings Panel"}
       </h2>
-      <ul className={styles.menu}>
+      <ul className={styles.sidebar_menu}>
         {props.nodesPanel ? (
           <NodesPanel />
-        ) : (
+        ) : (<Fragment>
+          <Label>Text</Label>
           <Input
             value={props.inputValue}
             onChange={(e) => props.updatedInputValue(e.target.value)}
           />
+          </Fragment>
         )}
       </ul>
     </div>
